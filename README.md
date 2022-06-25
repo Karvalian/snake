62ed660a8ecd5d0653de777eb617294f7135d8ed
karvalian <obaid8197@gmail.com>
2022-06-26 00:04:07 +0500

    Game Over

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple snake game made live on youtube/c/karvalian
+
+
+TODOs:
+  Create fail system
+  Quit key
+  
--- game.py
+++ game.py
@@ -31,10 +31,11 @@ clock = pygame.time.Clock()
 
 while running:
   dt = clock.tick(60)
-
   random.seed(time.time())
   keys = [K_d, K_a, K_w, K_s]
   pause = font.render(str("Paused"), True, ((255, 255,0)))
+  over = font.render(str("Game Over"), True, ((255, 255,0)))
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
@@ -42,6 +43,7 @@ while running:
     if event.type == pygame.KEYDOWN:
       lastkey = event.key
       
+  screenrect = screen.get_rect()
   if(lastkey == K_d):
     rect.x += speed*dt
   if(lastkey == K_a):
@@ -50,19 +52,23 @@ while running:
     rect.y-= speed*dt
   if(lastkey == K_s):
     rect.y+= speed*dt
-  rect.clamp_ip(screen.get_rect())
+  if not screenrect.contains(rect):
+    lastkey = K_l
+    score = 0
+    rect.clamp_ip(screen.get_rect())
   if rect.colliderect(foodrect):
     foodrect.x = random.randint(0,900-35)
     foodrect.y = random.randint(0,640-35)
     speed+=0.0001
     score+=1
-    
   screen.fill(bruh)
   screen.blit(snake, (rect.x,rect.y))
   text = font.render(str("Score: " + str(score)).encode(), True, ((255,255,0)))
   text2 = font.render(str("Made by Karvalian"), True, ((255,255,0)))
-  if(lastkey not in keys):
+  if((lastkey not in keys) & (lastkey!=K_l)):
     screen.blit(pause, (300, 0))
+  elif(lastkey==K_l):
+    screen.blit(over, (300, 0))
   screen.blit(text, (0, 0))
   screen.blit(text2, (550, 0))
   if(foodrect.x!=-1):
