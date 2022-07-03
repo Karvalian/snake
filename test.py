7403c2d1e4abf29c8983cf8857422dcea9a91631
Obaid Khan <42641723+Karvalian@users.noreply.github.com>
2022-07-03 16:41:26 +0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-URL = "THE URL THATS AVAILAIBLE FOR EVERYONE ON MY GITHUB REPOSITORY"
-data = {
-  'content': 'You got hacked by error',
-  'username': 'You must not share your information'
-}
-while True:
-  requests.post(data=data, url=URL)
